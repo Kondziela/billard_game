f2edf88e5ae5db2d7b5e7fb49e79811570029e28
Artur Kondziela <artur.kondziela@capgemini.com>
2016-11-22 00:15:24 +0100

    Lepsze rotacje kija

--- Assets/FizykaKij.js
+++ Assets/FizykaKij.js
@@ -4,13 +4,12 @@ var isStrike = false;
 var isBack = false;
 var countStrike = 0;
 
-function Start () {
-
-}
-
 function Update () {
 	var szybkosc = Time.deltaTime * 100;
 	var rotationVector = transform.rotation.eulerAngles;
+	var rotVec = [rotationVector.x, rotationVector.y, rotationVector.z];
+	Debug.Log("Rotation: " + rotVec[0] + rotVec[1] + rotVec[2]);
+	Debug.Log("Position: " + transform.position.ToString());
 	if(isStrike) {
 		if(countStrike < 10) {
 			transform.Translate(0, szybkosc, 0);
@@ -52,22 +51,102 @@ function Update () {
 	}
 	// rotate up
 	if(Input.GetKey ("i")) {
-		rotationVector.x += 3;
-		transform.rotation = Quaternion.Euler(rotationVector);
+		if(rotationVector.z > 93) {
+			rotationVector.z -= 3;
+			transform.rotation = Quaternion.Euler(rotationVector);
+		}
 	}
 	// rotate down
 	if(Input.GetKey ("k")) {
-		rotationVector.x -= 3;
+		rotationVector.z += 3;
 		transform.rotation = Quaternion.Euler(rotationVector);	
 	}
 	// rotate left
 	if(Input.GetKey ("j")) {
-		rotationVector.z += 3;
+		rotationVector.y -= 3;
 		transform.rotation = Quaternion.Euler(rotationVector);	
 	}
 	// rotate right
 	if(Input.GetKey ("l")) {
-		rotationVector.z -= 3;
+		rotationVector.y += 3;
 		transform.rotation = Quaternion.Euler(rotationVector);
 	}
 }
+
+/*function mnozMacierz31(A: float[,], V: float[]) : float[]
+{
+	var vec = new float[3];
+	vec[0] = A[0,0]*V[0] + A[0,1]*V[1] + A[0,2]*V[2];
+	vec[1] = A[1,0]*V[0] + A[1,1]*V[1] + A[1,2]*V[2];
+	vec[2] = A[2,0]*V[0] + A[2,1]*V[1] + A[2,2]*V[2];
+	return vec;
+}
+
+function mnozMacierze33(A: float[,], B: float[,]) : float[,]
+{
+	var table = new float[3,3];
+	table[0,0] = A[0,0]*B[0,0] + A[0,1]*B[1,0] + A[0,2]*B[2,0];
+	table[0,1] = A[0,0]*B[0,1] + A[0,1]*B[1,1] + A[0,2]*B[2,1];
+	table[0,2] = A[0,0]*B[0,2] + A[0,1]*B[1,2] + A[0,2]*B[2,2];
+	table[1,0] = A[1,0]*B[0,0] + A[1,1]*B[1,0] + A[1,2]*B[2,0];
+	table[1,1] = A[1,0]*B[0,1] + A[1,1]*B[1,1] + A[1,2]*B[2,1];
+	table[1,2] = A[1,0]*B[0,2] + A[1,1]*B[1,2] + A[1,2]*B[2,2];
+	table[2,0] = A[2,0]*B[0,0] + A[2,1]*B[1,0] + A[2,2]*B[2,0];
+	table[2,1] = A[2,0]*B[0,1] + A[2,1]*B[1,1] + A[2,2]*B[2,1];
+	table[2,2] = A[2,0]*B[0,2] + A[2,1]*B[1,2] + A[2,2]*B[2,2];
+	return table;
+}
+
+function macierzObrotuX(alfa: float) : float[,]
+{
+	var A = new float[3,3];
+	A[0,0] = 1.0;
+	A[0,1] = 0;
+	A[0,2] = 0;
+	A[1,0] = 0;
+	A[1,1] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	A[1,2] = -Mathf.Sin(Mathf.Deg2Rad*alfa);
+	A[2,0] = 0;
+	A[2,1] = Mathf.Sin(Mathf.Deg2Rad*alfa);
+	A[2,2] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	return A; 
+}
+
+function macierzObrotuY(alfa: float) : float[,]
+{
+	var B = new float[3,3];
+	B[0,0] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	B[0,1] = 0;
+	B[0,2] = Mathf.Sin(Mathf.Deg2Rad*alfa);
+	B[1,0] = 0;
+	B[1,1] = 1;
+	B[1,2] = 0;
+	B[2,0] = -Mathf.Sin(Mathf.Deg2Rad*alfa);
+	B[2,1] = 0;
+	B[2,2] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	return B;
+}
+
+function macierzObrotuZ(alfa: float) : float[,]
+{
+	var C = new float[3,3];
+	C[0,0] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	C[0,0] = -Mathf.Sin(Mathf.Deg2Rad*alfa);
+	C[0,0] = 0;
+	C[0,0] = Mathf.Sin(Mathf.Deg2Rad*alfa);
+	C[0,0] = Mathf.Cos(Mathf.Deg2Rad*alfa);
+	C[0,0] = 0;
+	C[0,0] = 0;
+	C[0,0] = 0;
+	C[0,0] = 1;
+	return C;
+}
+
+function przemiescOWektor(V: float[], rot: float[]) : Vector3
+{
+	var macierzObrX = macierzObrotuX(rot[0]);
+	var macierzObrY = macierzObrotuY(rot[1]);
+	var macierzObrZ = macierzObrotuZ(rot[2]);
+	var retVec = mnozMacierz31(mnozMacierze33(mnozMacierze33(macierzObrZ, macierzObrY), macierzObrX), V);
+	return new Vector3(retVec[0], retVec[1], retVec[2]);
+}*/
